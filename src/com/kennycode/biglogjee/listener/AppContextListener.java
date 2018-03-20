dc3e04ba65f2270ae9b722235bea30285d9d4ec1
Kenny Code <kenny030217@gmail.com>
2018-03-20 18:56:42 -0300

    Changed the name of project in .settings, added JNDIDataSource and MySQLDataSource, added PersonalSessionListener.

--- src/com/kennycode/biglogjee/listener/AppContextListener.java
+++ src/com/kennycode/biglogjee/listener/AppContextListener.java
@@ -1,5 +1,7 @@
 package com.kennycode.biglogjee.listener;
 
+import java.sql.SQLException;
+
 import javax.servlet.ServletContext;
 import javax.servlet.ServletContextEvent;
 import javax.servlet.ServletContextListener;
@@ -10,11 +12,16 @@ public class AppContextListener implements ServletContextListener {
 	public void contextInitialized(ServletContextEvent servletContextEvent) {
 		ServletContext ctx = servletContextEvent.getServletContext();
 		
-		String url = ctx.getInitParameter("DBURL");
-		String u = ctx.getInitParameter("DBUSER");
-		String p = ctx.getInitParameter("DBPWD");
-		// create database connection from init parameters and set it to context
-		DBConnectionManager dbManager = new DBConnectionManager(url, u, p);
+		//	String url = ctx.getInitParameter("dbURL");
+		//	String u = ctx.getInitParameter("dbUser");
+		//	String p = ctx.getInitParameter("dbPwd");
+		//  create database connection from init parameters and set it to context
+		//	DBConnectionManager dbManager = new DBConnectionManager(url, u, p);
+		
+		String JNDIDataSource = ctx.getInitParameter("JNDIDataSource");
+		
+		// change that to use the real implementation of dbManager*
+		DBConnectionManager dbManager = new DBConnectionManager(JNDIDataSource);
 		ctx.setAttribute("DBManager", dbManager);
 		System.out.println("Database connection initialized for Application.");
 	}
@@ -22,7 +29,11 @@ public class AppContextListener implements ServletContextListener {
 	public void contextDestroyed(ServletContextEvent servletContextEvent) {
 		ServletContext ctx = servletContextEvent.getServletContext();
 		DBConnectionManager dbManager = (DBConnectionManager) ctx.getAttribute("DBManager");
-		dbManager.closeConnection();
+		try {
+			dbManager.closeConnection();
+		} catch(SQLException e) {
+			e.printStackTrace();
+		}
 		System.out.println("Database connection closed for Application.");
 	}
 }
